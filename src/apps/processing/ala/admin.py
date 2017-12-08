a9397572273e90a6c1beedd6e8bd5d1f45349d8d
jirik <jirik@users.noreply.github.com>
2017-12-08 10:59:40 +0100

    Add list view and filters to ALA admin

--- src/apps/processing/ala/admin.py
+++ src/apps/processing/ala/admin.py
@@ -9,6 +9,20 @@ class SamplingFeatureAdmin(admin.ModelAdmin):
 
 
 class ObservationAdmin(admin.ModelAdmin):
+    list_display = (
+        'phenomenon_time',
+        'phenomenon_time_to',
+        'observed_property',
+        'feature_of_interest',
+        'procedure',
+        'result',
+    )
+    list_filter = (
+        'phenomenon_time',
+        ('observed_property', admin.RelatedOnlyFieldListFilter),
+        ('feature_of_interest', admin.RelatedOnlyFieldListFilter),
+        ('procedure', admin.RelatedOnlyFieldListFilter),
+    )
     readonly_fields = (
         'phenomenon_time',
         'phenomenon_time_to',
