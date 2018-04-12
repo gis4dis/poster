5debf354911f4e6a24a439a7e1527084e2c42fe3
AndyZ-Salz <zkyzkyex@gmail.com>
2018-04-12 15:16:06 +0200

    add K.Zang's test file

---
+++ apps/processing/ala/management/commands/ala_analyse_ZangTest.py
@@ -0,0 +1,10 @@
+import logging
+from django.core.management.base import BaseCommand
+from apps.processing.ala.util import util
+from dateutil.parser import parse
+from dateutil import relativedelta
+from datetime import date, timedelta
+
+logger = logging.getLogger(__name__)
+
+
