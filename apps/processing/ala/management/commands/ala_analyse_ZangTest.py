bd0e8fbff166039b292bb4f9ab31872a603e571e
AndyZ-Salz <zkyzkyex@gmail.com>
2018-04-17 11:21:42 +0200

    make save function as a method

--- apps/processing/ala/management/commands/ala_analyse_ZangTest.py
+++ apps/processing/ala/management/commands/ala_analyse_ZangTest.py
@@ -1,13 +1,7 @@
 import logging
 from django.core.management.base import BaseCommand
-from apps.processing.ala.util import util
-from dateutil.parser import parse
-from dateutil import relativedelta
-from datetime import date, timedelta
 from apps.common.models import Process, Property
 from apps.processing.ala.models import SamplingFeature, Observation
-import luminol
-from psycopg2.extras import DateTimeTZRange
 
 logger = logging.getLogger(__name__)
 
@@ -20,31 +14,39 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-
         station = SamplingFeature.objects.get(id_by_provider='11359205')
 
-        prop = Property.objects.get(name_id='air_temperature')
+        prop = Property.objects.get(name_id='precipitation')
 
         process = Process.objects.get(name_id='measure')
 
-        obss = Observation.objects.filter(
+        raw_obss = Observation.objects.filter(
             feature_of_interest=station
             , observed_property=prop
             , procedure=process
         )
-        process2 = Process.objects.get(name_id='anomaly')
 
-        for obj in obss:
-            result = 233
-            result_null_reason = ''
-            obs = Observation.objects.create(
-                phenomenon_time_range=obj.phenomenon_time_range,
-                observed_property=obj.observed_property,
-                feature_of_interest=obj.feature_of_interest,
-                procedure=process2,
-                result=result,
-                result_null_reason=result_null_reason,
-            )
-            obs.related_observations.set(obss)
+        score = []
 
-        print("finish")
+        for i in range(0, len(raw_obss)):
+            score.append(i)
+
+        # mainpart
+        anomaly_score_save(raw_obss, score)
+
+
+def anomaly_score_save(raw_obss, score):
+
+    anomaly_process = Process.objects.get(name_id='anomaly')
+
+    for i in range(0, len(raw_obss)):
+        new_obs = Observation.objects.create(
+            phenomenon_time_range=raw_obss[i].phenomenon_time_range,
+            observed_property=raw_obss[i].observed_property,
+            feature_of_interest=raw_obss[i].feature_of_interest,
+            procedure=anomaly_process,
+            result=score[i],
+            result_null_reason='',
+        )
+        new_obs.related_observations.set(raw_obss)
+    print("Saved successfully")
