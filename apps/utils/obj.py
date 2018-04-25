56e0c1ebd60e05e7e1f000d0be7ad987de74479d
jirik <jirik@users.noreply.github.com>
2018-04-25 09:54:32 +0200

    Use update_or_create in utils.obj.get_or_create_objs

--- apps/utils/obj.py
+++ apps/utils/obj.py
@@ -1,15 +1,15 @@
-def get_or_create_obj(the_class, obj_def, unique_attr):
+def update_or_create(the_class, obj_def, unique_attr):
     goc_args = {
         unique_attr: obj_def[0],
         'defaults': obj_def[1],
     }
-    obj, _ = the_class.objects.get_or_create(**goc_args)
+    obj, _ = the_class.objects.update_or_create(**goc_args)
     return obj
 
 
 def get_or_create_objs(the_class, objs_def, unique_attr):
     objs_map = map(
-        lambda obj_def: get_or_create_obj(the_class, obj_def, unique_attr),
+        lambda obj_def: update_or_create(the_class, obj_def, unique_attr),
         objs_def
     )
     return list(objs_map)
