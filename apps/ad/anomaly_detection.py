gis4dis/poster

commit 97c456805a3157cc1e65405c2db7df04500c50d9

Reflected changes to process specification in settings and AD. Resolves #43

apps/ad/anomaly_detection.py
@@ -8,7 +8,7 @@ from luminol.anomaly_detector import AnomalyDetector
 def get_timeseries(observed_property, observation_provider_model, feature_of_interest, phenomenon_time_range):
     frequency = settings.APPLICATION_MC.PROPERTIES[observed_property.name_id]["value_frequency"]
     process = Process.objects.get(
-        name_id=settings.APPLICATION_MC.PROPERTIES[observed_property.name_id]["process"])
+        name_id=settings.APPLICATION_MC.PROPERTIES[observed_property.name_id][observation_provider_model]["process"])
     observation_model_name = f"{observation_provider_model.__module__}.{observation_provider_model.__name__}"
     timezone = phenomenon_time_range.lower.tzinfo
 

apps/mc/settings.py
@@ -18,9 +18,6 @@ PROPERTIES = {
 
         # mandatory, number of seconds
         'value_frequency': 3600,
-
-        # mandatory, name_id of common.Process
-        'process': 'avg_hour',
     },
 
     'ground_air_temperature': {
@@ -30,7 +27,6 @@ PROPERTIES = {
             },
         },
         'value_frequency': 3600,
-        'process': 'avg_hour',
     },
 
 }

commit 8c5adcacd9108078a6f4603c3693ae978d16d114

API REST - timeseries + properties  (#47)
* API REST - timeseries + properties  (#47)

* Fix KeyError caused by #52

Close #32

apps/ad/anomaly_detection.py
@@ -7,8 +7,11 @@ from luminol.anomaly_detector import AnomalyDetector
 
 def get_timeseries(observed_property, observation_provider_model, feature_of_interest, phenomenon_time_range):
     frequency = settings.APPLICATION_MC.PROPERTIES[observed_property.name_id]["value_frequency"]
+    observation_provider_model_name = observation_provider_model.__module__ \
+                                      + '.' \
+                                      + observation_provider_model.__name__
     process = Process.objects.get(
-        name_id=settings.APPLICATION_MC.PROPERTIES[observed_property.name_id][observation_provider_model]["process"])
+        name_id=settings.APPLICATION_MC.PROPERTIES[observed_property.name_id]['observation_providers'][observation_provider_model_name]["process"])
     observation_model_name = f"{observation_provider_model.__module__}.{observation_provider_model.__name__}"
     timezone = phenomenon_time_range.lower.tzinfo
 
@@ -64,12 +67,12 @@ def get_timeseries(observed_property, observation_provider_model, feature_of_int
 
     dt = result_time_range.upper - result_time_range.lower
 
-    print(dt, frequency, dt/frequency, range(1, int(dt/frequency)))
+    #print(dt, frequency, dt/frequency, range(1, int(dt/frequency)))
 
     for i in range(1, int(dt/frequency)):
         t = result_time_range.lower + i * frequency
         if t not in obs_reduced or obs_reduced[t] is None:
-            print(i, t)
+            #print(i, t)
             obs_reduced[t] = None
             anomalyScore.insert(i, None)
 

apps/common/models.py
@@ -2,7 +2,6 @@
 # from django.utils.timezone import localtime
 from django.contrib.gis.db import models
 from django.contrib.postgres import fields as pgmodels
-
 from apps.utils.time import format_delta
 
 

apps/common/serializers.py
@@ -1,8 +1,0 @@
-from rest_framework import serializers
-from apps.common.models import Property
-
-
-class PropertySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Property
-        fields = ('name_id', 'name', 'unit')

apps/common/views.py
@@ -1,12 +1,0 @@
-from rest_framework import viewsets
-from apps.common.serializers import PropertySerializer
-from apps.common.models import Property
-
-from django.conf import settings
-
-
-class PropertyViewSet(viewsets.ReadOnlyModelViewSet):
-    prop_names = settings.APPLICATION_MC.PROPERTIES.keys()
-    queryset = Property.objects.filter(name_id__in=prop_names)
-    serializer_class = PropertySerializer
-

apps/mc/models.py
@@ -1,0 +1,35 @@
+# from django.db import models
+# from django.utils.timezone import localtime
+from django.contrib.gis.db import models
+from django.db.models.fields import DecimalField
+from django.contrib.postgres.fields import ArrayField
+
+
+class TimeSeriesFeature(models.Model):
+    id_by_provider = models.CharField(
+        help_text="ID of the station used by provider.",
+        max_length=50,
+        editable=False
+    )
+
+    name = models.CharField(
+        help_text="Human-readable name of the station.",
+        max_length=50
+    )
+
+    geometry = models.PointField(
+        help_text="Spatial information about feature."
+    )
+
+    property_values = ArrayField(DecimalField(decimal_places=5, max_digits=15), null=True, blank=True)
+    property_anomaly_rates = ArrayField(DecimalField(decimal_places=5, max_digits=15), null=True, blank=True)
+    phenomenon_time_from = models.DateTimeField()
+    phenomenon_time_to = models.DateTimeField()
+    value_index_shift = models.IntegerField()
+
+    class Meta:
+        managed = False
+
+    def __str__(self):
+        return self.name
+

apps/mc/serializers.py
@@ -1,0 +1,31 @@
+from rest_framework import serializers
+from apps.common.models import Property
+from apps.mc.models import TimeSeriesFeature
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = ('name_id', 'name', 'unit')
+
+
+class TimeSeriesFeatureSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = TimeSeriesFeature
+        geo_field = "geometry"
+        fields = (
+            'id',
+            'id_by_provider',
+            'name',
+            'value_index_shift',
+            'property_values',
+            'property_anomaly_rates'
+        )
+
+
+class TimeSeriesSerializer(serializers.Serializer):
+    phenomenon_time_from = serializers.DateTimeField()
+    phenomenon_time_to = serializers.DateTimeField()
+    value_frequency = serializers.IntegerField()
+    feature_collection = TimeSeriesFeatureSerializer(many=True)

apps/mc/settings.py
@@ -29,4 +29,22 @@ PROPERTIES = {
         'value_frequency': 3600,
     },
 
+    'stream_flow': {
+        'observation_providers': {
+            'apps.processing.pmo.models.WatercourseObservation': {
+                'process': 'avg_hour',
+            },
+        },
+        'value_frequency': 3600,
+    },
+
+    'water_level': {
+        'observation_providers': {
+            'apps.processing.pmo.models.WatercourseObservation': {
+                'process': 'avg_hour',
+            },
+        },
+        'value_frequency': 3600,
+    },
+
 }

apps/mc/views.py
@@ -1,0 +1,242 @@
+from importlib import import_module
+from rest_framework import viewsets
+from django.contrib.gis.geos import Polygon
+from rest_framework.exceptions import APIException
+from django.conf import settings
+from dateutil.parser import parse
+from dateutil import relativedelta
+from psycopg2.extras import DateTimeTZRange
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from apps.mc.serializers import PropertySerializer, TimeSeriesSerializer
+from apps.common.models import Property
+from apps.mc.models import TimeSeriesFeature
+from apps.ad.anomaly_detection import get_timeseries
+
+
+def import_models(path):
+    provider_module = None
+    provider_model = None
+    error_message = None
+    try:
+        path = path.rsplit('.', 1)
+        provider_module = import_module(path[0])
+        provider_model = getattr(provider_module, path[1])
+        return provider_model, provider_module, error_message
+    except ModuleNotFoundError as e:
+        error_message = 'module not found'
+        return provider_model, provider_module, error_message
+    except AttributeError as e:
+        error_message = 'function not found'
+        return provider_model, provider_module, error_message
+
+
+def parse_date_range(from_string, to_string):
+    if from_string:
+        try:
+            day_from = parse(from_string)
+        except ValueError as e:
+            raise APIException("Phenomenon_date_from is not valid")
+
+    if to_string:
+        try:
+            day_to = parse(to_string)
+            day_to = day_to + relativedelta.relativedelta(days=1)
+        except ValueError as e:
+            raise APIException("Phenomenon_date_to is not valid")
+
+    if day_from > day_to:
+        raise APIException("Phenomenon_date_from bound must be less than or equal phenomenon_date_to")
+
+    time_range_boundary = '[]' if day_from == day_to else '[)'
+
+    pt_range = DateTimeTZRange(
+        day_from, day_to, time_range_boundary)
+
+    return  pt_range, day_from, day_to
+
+
+def float_bbox_param(value):
+    try:
+        float_val = float(value)
+        return float_val
+    except:
+        raise APIException("BBOX param float conversion error: %s" % value)
+
+
+def validate_bbox_values(bbox_array):
+    if bbox_array[0] >= bbox_array[2]:
+        raise APIException("BBOX minx is not < maxx")
+
+    if bbox_array[1] >= bbox_array[3]:
+        raise APIException("BBOX miny is not < maxy")
+
+
+def parse_bbox(bbox_string):
+    bbox_parts = bbox_string.rsplit(',')
+
+    if len(bbox_parts) != 4:
+        raise APIException("BBOX 4 parameters required")
+
+    x_min = float_bbox_param(bbox_parts[0])
+    y_min = float_bbox_param(bbox_parts[1])
+    x_max = float_bbox_param(bbox_parts[2])
+    y_max = float_bbox_param(bbox_parts[3])
+
+    if x_min and y_min and x_max and y_max:
+        bbox = (x_min, y_min, x_max, y_max)
+        validate_bbox_values(bbox)
+        geom_bbox = Polygon.from_bbox(bbox)
+    else:
+        raise APIException("Error in passed query parameter: bbox")
+
+    return geom_bbox
+
+
+def validate_time_series_feature(item, time_series_from, time_series_to, value_frequency):
+    if time_series_from and time_series_to:
+        time_series_from_s = int(round(time_series_from.timestamp() * 1000))
+        time_series_to_s = int(round(time_series_to.timestamp() * 1000))
+
+        if len(item.property_values) != len(item.property_anomaly_rates):
+            raise APIException(
+                "Error: feature.property_values.length !== feature.property_anomaly_rates.length")
+
+        if time_series_from_s % value_frequency != 0:
+            raise APIException(
+                "Error: OUT.phenomenon_time_from::seconds % OUT.value_frequency !== 0")
+
+        if (time_series_to_s - time_series_from_s) % value_frequency != 0:
+            raise APIException(
+                "Error: (OUT.phenomenon_time_to::seconds - OUT.phenomenon_time_from::seconds) % OUT.value_frequency != 0")
+
+        #TODO vyresit problem se spravnym poctem vracenych hodnot a potom odkomentovat
+        '''
+        values_max_count = (time_series_to - time_series_from).total_seconds() / value_frequency
+
+        if (len(item.property_values) + item.value_index_shift) > values_max_count:
+            raise APIException(
+                "Error: feature.property_values.length + feature.value_index_shift > (phenomenon_time_to::seconds - phenomenon_time_from::seconds) / value_frequency")
+        '''
+
+
+class PropertyViewSet(viewsets.ReadOnlyModelViewSet):
+    prop_names = settings.APPLICATION_MC.PROPERTIES.keys()
+    queryset = Property.objects.filter(name_id__in=prop_names)
+    serializer_class = PropertySerializer
+
+
+#TODO otestovat vice provideru v ramci jedne charakteristiky v configu
+# http://localhost:8000/api/v1/timeseries?name_id=water_level&phenomenon_date_from=2017-01-20&phenomenon_date_to=2017-01-27&bbox=1826997.8501,6306589.8927,1846565.7293,6521189.3651
+# http://localhost:8000/api/v1/timeseries?name_id=water_level&phenomenon_date_from=2017-01-20&phenomenon_date_to=2017-01-27
+# http://localhost:8000/api/v1/timeseries?name_id=air_temperature&phenomenon_date_from=2017-01-20&phenomenon_date_to=2017-01-27&bbox=1826997.8501,6306589.8927,1846565.7293,6521189.3651
+# http://localhost:8000/api/v1/timeseries?name_id=air_temperature&phenomenon_date_from=2018-01-20&phenomenon_date_to=2018-09-27
+
+class TimeSeriesViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        if 'name_id' in request.GET:
+            name_id = request.GET['name_id']
+        else:
+            raise APIException("Parameter name_id is required")
+
+        if 'phenomenon_date_from' in request.GET:
+            phenomenon_date_from = request.GET['phenomenon_date_from']
+        else:
+            raise APIException("Parameter phenomenon_date_from is required")
+
+        if 'phenomenon_date_to' in request.GET:
+            phenomenon_date_to = request.GET['phenomenon_date_to']
+        else:
+            raise APIException("Parameter phenomenon_date_to is required")
+
+        pt_range, day_from, day_to = parse_date_range(phenomenon_date_from, phenomenon_date_to)
+
+        geom_bbox = None
+        if 'bbox' in request.GET:
+            bbox = request.GET['bbox']
+            geom_bbox = parse_bbox(bbox)
+
+        if not (name_id in settings.APPLICATION_MC.PROPERTIES):
+            raise APIException("name_id not found in config")
+
+        config_prop = settings.APPLICATION_MC.PROPERTIES[name_id]
+        config_observation_providers = config_prop['observation_providers']
+
+        for key in config_observation_providers:
+            time_series_list = []
+            provider_module, provider_model, error_message = import_models(key)
+            if error_message:
+                raise APIException("Importing error - %s : %s" % (key, error_message))
+
+            path = key.rsplit('.', 1)
+            provider_module = import_module(path[0])
+            provider_model = getattr(provider_module, path[1])
+
+            feature_of_interest_model = provider_model._meta.get_field('feature_of_interest').remote_field.model
+
+            if geom_bbox:
+                all_features = feature_of_interest_model.objects.filter(geometry__intersects=geom_bbox)
+            else:
+                all_features = feature_of_interest_model.objects.all()
+
+            phenomenon_time_from = None
+            phenomenon_time_to = None
+            value_frequency = None
+
+            for item in all_features:
+                ts = get_timeseries(
+                    observed_property=Property.objects.get(name_id=name_id),
+                    observation_provider_model=provider_model,
+                    feature_of_interest=item,
+                    phenomenon_time_range=pt_range)
+
+                if not phenomenon_time_from or phenomenon_time_from > ts['phenomenon_time_range'].lower:
+                    phenomenon_time_from = ts['phenomenon_time_range'].lower
+
+                if not phenomenon_time_to or phenomenon_time_to > ts['phenomenon_time_range'].upper:
+                    phenomenon_time_to = ts['phenomenon_time_range'].upper
+
+                if not value_frequency:
+                    value_frequency = ts['value_frequency']
+
+                f = TimeSeriesFeature(
+                    id=item.id,
+                    id_by_provider=item.id_by_provider,
+                    name=item.name,
+                    geometry=item.geometry,
+                    property_values=ts['property_values'],
+                    property_anomaly_rates=ts['property_anomaly_rates'],
+                    value_index_shift=None,
+                    phenomenon_time_from=ts['phenomenon_time_range'].lower,
+                    phenomenon_time_to=ts['phenomenon_time_range'].upper
+                )
+                time_series_list.append(f)
+
+            for item in time_series_list:
+                if phenomenon_time_from and item.phenomenon_time_from:
+                    diff = phenomenon_time_from - item.phenomenon_time_from
+                    value_index_shift = round(abs(diff.total_seconds()) / value_frequency)
+                    item.value_index_shift = value_index_shift
+
+                validate_time_series_feature(item, phenomenon_time_from, phenomenon_time_to, value_frequency)
+
+            response_data = {
+                'phenomenon_time_from': phenomenon_time_from,
+                'phenomenon_time_to': phenomenon_time_to,
+                'value_frequency': value_frequency,
+                'feature_collection': time_series_list
+            }
+
+        results = TimeSeriesSerializer(response_data).data
+        return Response(results)
+
+
+class PropertyViewSet(viewsets.ReadOnlyModelViewSet):
+    prop_names = settings.APPLICATION_MC.PROPERTIES.keys()
+    queryset = Property.objects.filter(name_id__in=prop_names)
+    serializer_class = PropertySerializer
+
+
+

poster/settings.py
@@ -69,7 +69,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'  # Whit
 LOG_LEVEL = env('DJANGO_LOG_LEVEL', default='ERROR')
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -89,6 +88,7 @@ INSTALLED_APPS = [
 
     'apps.common',
     'apps.utils',
+    'apps.mc',
 
     'apps.importing',
 
@@ -98,6 +98,7 @@ INSTALLED_APPS = [
     'apps.processing.rsd',
     'apps.processing.ozp',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -177,4 +178,8 @@ LOGGING = {
             'level': LOG_LEVEL,
         },
     },
+}
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
 }

poster/urls.py
@@ -1,27 +1,13 @@
-"""poster URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls import url
 from rest_framework import routers
-from apps.common import views
+from apps.mc import views
 
 router = routers.DefaultRouter()
 router.register(r'properties', views.PropertyViewSet)
+router.register(r'timeseries', views.TimeSeriesViewSet, base_name='timeseries')
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),