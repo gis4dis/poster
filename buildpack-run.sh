03919fca63a932c3efc47b4bf8967c0e081835ea
Pavel Grochal <darkless69@gmail.com>
2018-08-01 14:58:11 +0200

    added debugging logs

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+echo "DIR: /tmp/app/"
 ls -la /tmp/app/
 echo "------------------------"
 
+echo "DIR: /tmp/app/modules/"
+ls -la /tmp/app/modules/
+echo "------------------------"
+
+echo "DIR: /tmp/app/modules/mc-client/"
+ls -la /tmp/app/modules/mc-client/
+echo "------------------------"
+
+echo "DIR: /tmp/build/"
 ls -la /tmp/build/
 echo "------------------------"
 
+echo "DIR: /tmp/build/app/"
 ls -la /tmp/build/app/
 echo "------------------------"
 
+echo "DIR: /tmp/build/app/modules/"
 ls -la /tmp/build/app/modules
 echo "------------------------"
 
+echo "DIR: /tmp/build/app/modules/mc-client/"
 ls -la /tmp/build/app/modules/mc-client/
 echo "------------------------"
