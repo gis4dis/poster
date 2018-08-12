85c240a1534318657ba2ec752e31fc6ccbfccd05
darkless <darkless69@gmail.com>
2018-08-12 19:15:46 +0200

    updated buildpack to proper version

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /app/modules/mc-client
 
-curl https://github.com/gis4dis/mc-client/releases/download/v1.3/release.tar.gz | tar xz
+curl -L https://github.com/gis4dis/mc-client/releases/download/v1.4/release.tar.gz | tar xz
