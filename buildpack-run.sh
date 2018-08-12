bbb002f74244f59427da61b842c0be7de1c58314
darkless <darkless69@gmail.com>
2018-08-12 19:32:00 +0200

    updated buildpack to verbose

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /app/modules/mc-client
 
-curl -L https://github.com/gis4dis/mc-client/releases/download/v1.4/release.tar.gz | tar xz
+curl -L https://github.com/gis4dis/mc-client/releases/download/v1.4/release.tar.gz | tar xvz
