da8ae060d605a9de494c74e793aa252ef410ee16
jirik <jirik@users.noreply.github.com>
2019-05-22 10:22:24 +0200

    Add mc-client v1.11

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -6,4 +6,4 @@ cd modules/mc-client
 pwd
 ls -la
 
-curl -L https://github.com/gis4dis/mc-client/releases/download/v1.9/release.tar.gz | tar xvz
+curl -L https://github.com/gis4dis/mc-client/releases/download/v1.11/release.tar.gz | tar xvz
